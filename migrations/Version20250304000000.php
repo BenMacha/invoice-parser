7ca6b5eafd3c113ec357efd654796f62b29019b4
abenmecha <contact@benmacha.tn>
2025-03-09 11:57:06 +0100

    update Invoice.php add miagration

---
+++ migrations/Version20250304000000.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250304000000 extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE invoice ALTER name TYPE VARCHAR(75)');
+        $this->addSql('ALTER TABLE invoice ALTER currency TYPE VARCHAR(3)');
+        $this->addSql('ALTER TABLE invoice ALTER invoice_date SET NOT NULL');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE invoice DROP invoice_date');
+    }
+}
--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -4,20 +4,74 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Repository\InvoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use DateTime;
 
-#[ORM\Entity(repositoryClass: 'App\Repository\InvoiceRepository')]
+#[ORM\Entity(repositoryClass: InvoiceRepository::class)]
 class Invoice
 {
     #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
-    public int $id;
+    private int $id;
 
-    #[ORM\Column(type: 'string')]
-    public string $name;
+    #[ORM\Column(type: 'string', length: 75)]
+    private string $name;
 
     #[ORM\Column(type: 'float')]
-    public float $amount;
+    private float $amount;
 
-    #[ORM\Column(type: 'string')]
-    public string $currency;
+    #[ORM\Column(type: 'string', length: 3)]
+    private string $currency;
+
+    #[ORM\Column(type: 'date')]
+    private DateTime $invoiceDate;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): self
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    public function setCurrency(string $currency): self
+    {
+        $this->currency = $currency;
+        return $this;
+    }
+
+    public function getInvoiceDate(): DateTime
+    {
+        return $this->invoiceDate;
+    }
+
+    public function setInvoiceDate(DateTime $invoiceDate): self
+    {
+        $this->invoiceDate = $invoiceDate;
+        return $this;
+    }
 }
